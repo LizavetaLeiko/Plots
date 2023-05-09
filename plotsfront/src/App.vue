feb671cb5861b431a9b12af669cae3ec6ce3abaa
LizavetaLeiko <lizaleiko3@gmail.com>
2023-05-09 11:19:49 +0300

    init front with map

---
+++ plotsfront/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <PlotsMap/>
+</template>
+
+<script>
+import PlotsMap from './components/map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PlotsMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+}
+</style>
---
+++ plotsfront/src/components/map.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="map-container">
+    <div ref="maps" class="map"></div>
+  </div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+
+export default {
+  name: 'PlotsMap',
+  data () {
+    return {
+      maps: null,
+      // mapsIsInitialized: false
+    }
+  },
+  mounted () {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGl6YWxlaWtvMyIsImEiOiJjbGhmN2JycDMxc3gxM2xudWZhOWIxbHVpIn0.oLNof4WsUYpXqqZka0qpxg'
+    this.maps = new mapboxgl.Map({
+      container: this.$refs.maps,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [ 27.561831, 53.902284],
+      zoom: 6 
+    })
+    // this.maps.on('load', () => {
+    // //   this.isMapsInitialized = true
+    // })
+  },
+  
+}
+</script>
+
+<style scoped>
+.map-container {
+  width: 100%;
+  height: 100vh;
+}
+.map {
+  width: 100%;
+  height: 100%;
+}
+</style>
