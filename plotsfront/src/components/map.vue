204382791241583b125d8d29029e2d7829f99c3f
LizavetaLeiko <lizaleiko3@gmail.com>
2023-05-09 11:48:43 +0300

    feat: displaying plots on map

--- plotsfront/src/components/map.vue
+++ plotsfront/src/components/map.vue
@@ -6,16 +6,17 @@
 
 <script>
 import mapboxgl from 'mapbox-gl'
+import axios from 'axios'
 
 export default {
   name: 'PlotsMap',
   data () {
     return {
       maps: null,
-      // mapsIsInitialized: false
+      plots: [],
     }
   },
-  mounted () {
+  async mounted () {
     mapboxgl.accessToken = 'pk.eyJ1IjoibGl6YWxlaWtvMyIsImEiOiJjbGhmN2JycDMxc3gxM2xudWZhOWIxbHVpIn0.oLNof4WsUYpXqqZka0qpxg'
     this.maps = new mapboxgl.Map({
       container: this.$refs.maps,
@@ -23,11 +24,52 @@ export default {
       center: [ 27.561831, 53.902284],
       zoom: 6 
     })
-    // this.maps.on('load', () => {
-    // //   this.isMapsInitialized = true
-    // })
+
+    this.maps.on('load', () => {
+      this.addPlots()
+    })
+    axios.get('http://localhost:1337/api/plots?populate=points', {
+      headers: {
+        Authorization: 'Bearer e415b0c58749f47b0a303aad4b30b13ed53f39b53db271e6d50f1bb8ef61ba8f29e8d92346df6eae7eda852c2bd9e3c895451913e67439bd2101052fa7348466bd775e54747a273b84222bc23137000bf29f823530f54fece46b4af9d18ad05d1aa9d3b763e392e8970c4be563f0483a2af68a73e1175c6dbb920b92827e3f05'
+      }
+    })
+    .then(response => {
+      this.plots = response.data.data
+      this.addPlots()
+    })
+    .catch(error => {
+      console.log(error)
+    })
   },
-  
+  methods: {
+    addPlots() {
+      this.plots.forEach(plot => {
+        const coordinates = plot.attributes.points.data.map(point => [point.attributes.longitude, point.attributes.latitude])
+        console.log(plot.attributes.points.data)
+        if (!this.maps.getLayer(plot.attributes.plot_id)) { 
+        this.maps.addLayer({
+          id: plot.attributes.plot_id, 
+          type: 'fill',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'Polygon',
+                coordinates: [coordinates]
+              }
+            }
+          },
+          paint: {
+            'fill-color': '#DF1717',
+            'fill-opacity': 0.6
+          }
+        })
+      }
+      })
+    },
+  }
 }
 </script>
 
